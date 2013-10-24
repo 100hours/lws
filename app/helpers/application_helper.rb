f57bfad7ba733a53f0cf66c2b89786d2ec385042
Riccardo Cambiassi <r.cambiassi@gmail.com>
2013-10-24 09:23:01 +0100

    add page title and active sections in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     return title unless title.blank?
     "<em>Untitled</em>"
   end
+
+  def active_on(title)
+    "active" if content_for(:page_title) == title
+  end
 end
