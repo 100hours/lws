0172ba3fb7bcb5edfc666b3ca7f12ca3d73f352d
Riccardo Cambiassi <r.cambiassi@gmail.com>
2013-10-25 16:41:50 +0100

    removing switch for requester notification

--- app/models/document.rb
+++ app/models/document.rb
@@ -8,7 +8,6 @@ class Document < ActiveRecord::Base
   end
 
   def notify_requester
-    return true unless ENV['DELIVER_NOTIFICATIONS'] == "YES"
     DocumentNotifier.writing_started_on(self).deliver unless self.requester.blank?
   end
   def to_pusher
