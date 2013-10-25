b64a7cf6223e2866c5a8f91b4e968470d0a86181
Riccardo Cambiassi <r.cambiassi@gmail.com>
2013-10-25 03:08:56 +0100

    add logging when sending emails

--- app/mailers/document_notifier.rb
+++ app/mailers/document_notifier.rb
@@ -1,6 +1,7 @@
 class DocumentNotifier < ActionMailer::Base
   def writing_started_on(document)
     @document = document
+    logger.warn("Sending a notification to #{document.requester}")
     mail(to: document.requester,
          from: "no-reply@livewritingseries.com",
          subject: "[LiveWritingSeries] Work on the document you requested has started")
