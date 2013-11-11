250997dccffc7d46a0a91ad2dda240a7335574e5
Alex Heeton <alex@heeton.me>
2013-11-11 17:36:37 +0000

    wip

--- app/models/document.rb
+++ app/models/document.rb
@@ -10,13 +10,5 @@ class Document < ActiveRecord::Base
   def notify_requester
     DocumentNotifier.writing_started_on(self).deliver unless self.requester.blank?
   end
-  def to_pusher
-    {
-      id:         self.id.to_s,
-      title:      self.title,
-      body:       self.body,
-      updated_at: self.updated_at
-    }
-  end
 
 end
